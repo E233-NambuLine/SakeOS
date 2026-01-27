a807ad6875d7b64e768d3dc9399023e8013f255b
GT-R50_MA <33rdaisuke@gmail.com>
2026-01-28 07:51:16 +0900

    krnlx86.asm の更新

--- krnlx86.asm
+++ krnlx86.asm
@@ -1,13 +1,52 @@
-[org 0x1000]
+[org 0x10000]
 [bits 32]
 
-start:
-mov eax, 0x00008365
-mov ebx, eax
+section .bss
+draw_x: resb 4
+draw_y: resb 4
+readpos: resb 6
+mem_pos: resb 6
+sector: resb 6
+color_r: resb 1
+color_g: resb 1
+color_b : resb 1
+draw_a: resb 1
+excldr_ver: resb 1
+vram_addr: resb 12
+draw_vram_addr resb 12
 
-sofs:
-cmp ah, 0x02
-je sofs_read
-cmp ah, 0x03
-je sofs_write
+section .text
+start:
+mov word [excldr_ver], 0x0A
+cli
+hlt
+
+draw_rect:
+call reg_push
+draw:
+mov ebx, [vram_addr]
+mov ecx, [draw_x]
+mov eax, [ebx+ecx]
+mov dword [draw_vram_addr], eax
+
+call reg_pop
+ret
+
+reg_push:
+push eax
+push ebx
+push ecx
+push edi
+push edx
+push esi
+ret
+
+reg_pop:
+pop esi
+pop edx
+pop edi
+pop ecx
+pop ebx
+pop eax
+ret
 
